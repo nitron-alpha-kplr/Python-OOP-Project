9c5b62ffb89f1e3db71195da686f506a91ec866a
nitron-alpha-kplr <124895759+nitron-alpha-kplr@users.noreply.github.com>
2023-03-09 10:10:10 +0000

    serge

--- exercices/07.unit_test/test.py
+++ exercices/07.unit_test/test.py
@@ -89,8 +89,31 @@ sys.path.extend(['.','..'])
 if __name__ == '__main__':
     unittest.main()
 
-     fruits = ['apple', 'baname', 'cherry']
+       fruits = ['apple', 'baname', 'cherry']
 
      self.assertin('apple',fruits)
+    
  
-        
+    import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()   
+
+
+    self.assertEqual('hello'.upper(), 'HELLO') 
